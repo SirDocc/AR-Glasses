6d15fd3913d0e756c63f7da6eb1a0f6530691db1
Sir <91761658+SirDocc@users.noreply.github.com>
2024-09-17 14:17:52 +0100

    better offset

--- Glasses_Code/Glasses_Code.ino
+++ Glasses_Code/Glasses_Code.ino
@@ -38,8 +38,8 @@ void drawcenterlines(void){
 }
 
 void drawarrowup(void){
-  int ox = 35; //offset X
-  int oy = 25; //offset Y
+  int ox = 45; //offset X
+  int oy = 20; //offset Y
   display.fillTriangle(10+ox, 0+oy, 0+ox, 10+oy, 20+ox, 10+oy, WHITE);
   display.fillRect(5+ox, 10+oy, 10, 10, WHITE);
   //Triangle Size: 10, 0, 0, 10, 20, 10
