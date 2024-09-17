29b7060bbc1ed847aa5132f4e05c7b3a7a5345a4
Sir <91761658+SirDocc@users.noreply.github.com>
2024-09-17 13:11:26 +0100

    missing ;

--- Glasses_Code/Glasses_Code.ino
+++ Glasses_Code/Glasses_Code.ino
@@ -39,5 +39,5 @@ void drawcenterlines(void){
 
 void drawarrowup(void){
   display.drawTriangle(6, 1, 1, 5, 10, 5, WHITE);
-  display.drawRect(5, 5, 5, 5, WHITE)
+  display.drawRect(5, 5, 5, 5, WHITE);
 }
